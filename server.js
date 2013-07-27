adec7bba31955c82edd656d965805cfeadc56e5b
Krishnan Chandra <krishnan.chandra@gmail.com>
2013-07-27 02:09:52 -0700

    pre-merge

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var app = express();
-
-var port = process.env.PORT || 3000;
-app.listen(port);
-console.log('Listening on port 3000');
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 app.use(express.static(__dirname + '/static'));
 app.use(express.bodyParser());
 
 app.get('/', function(req, res) {
-	res.sendfile('index.html');
+	res.render('index.ejs');
 });
 
 app.post('/showDirections', function(req, res) {
 	var start = req.body.start;
 	var end = req.body.end;
-	
+	res.sendfile('maps.ejs', {"start": start, "end": end});
 });
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Listening on port 3000');
--- static/js/navigate.js
+++ static/js/navigate.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
-    $("#dir1").val("53 Wild Rose Drive, Andover, MA");
-    $("#dir2").val("14 Bridle Path Road, Andover, MA");
+    $("#dir1").val("2029 Stierlin Court, Mountain View, CA");
+    $("#dir2").val("701 First Ave, Sunnyvale, CA");
 
     $("#gobtn").click(function() {
-        var input1 = $("#dir1").val();
-        var input2 = $("#dir2").val();
         $("#locForm").submit();
     });
 
