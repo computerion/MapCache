e7a204196d3b7bf665514570ca35a06d52a845bb
Daniel Hu <dhu@linkedin.com>
2013-07-27 02:10:02 -0700

    stuff

--- static/js/navigate.js
+++ static/js/navigate.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $("#dir1").val("53 Wild Rose Drive, Andover, MA");
     $("#dir2").val("14 Bridle Path Road, Andover, MA");
 
+    var dirType = "driving";
     $("#gobtn").click(function() {
         var input1 = $("#dir1").val();
         var input2 = $("#dir2").val();
@@ -9,8 +10,15 @@ $(document).ready(function() {
     });
 
     $(".optionsBtn").click(function(){
-        $("#mode").val($(this).val.toUpperCase());
+        $("#mode").val($(this).val().toUpperCase());
+        $(".optionsBtn").removeClass("optionsBtn-selected");
+        $(this).addClass("optionsBtn-selected");
+        dirType = $(this).id;
+        console.log($(this));
+        console.log(dirType);
     })
+
+    $("#driving").addClass("optionsBtn-selected");
 
     $(".dir").keypress(function(e) {
         if (e.which == 13) {
