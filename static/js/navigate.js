d23e0ede41eb6d12cb733b2300f4e8992c231821
Daniel Hu <dhu@linkedin.com>
2013-07-27 01:45:43 -0700

    ui stuff

--- static/js/navigate.js
+++ static/js/navigate.js
@@ -1,31 +1,15 @@
 $(document).ready(function() {
     $("#dir1").val("53 Wild Rose Drive, Andover, MA");
     $("#dir2").val("14 Bridle Path Road, Andover, MA");
-    google.maps.visualRefresh = true;
-    var directionsDisplay;
-    var directionsService = new google.maps.DirectionsService();
-    var map;
-    var dirType = google.maps.DirectionsTravelMode.DRIVING;
-
-    var mapOptions = {
-        zoom: 8,
-        center: new google.maps.LatLng(-34.397, 150.644),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    directionsDisplay = new google.maps.DirectionsRenderer();
-
-    directionsDisplay.setMap(map);
 
     $("#gobtn").click(function() {
         var input1 = $("#dir1").val();
         var input2 = $("#dir2").val();
-        $("locForm").submit();
+        $("#locForm").submit();
     });
 
     $(".optionsBtn").click(function(){
-        dirType = google.maps.DirectionsTravelMode[$(this).text().toUpperCase()];
-        console.log(dirType);
+        $("#mode").val($(this).val.toUpperCase());
     })
 
     $(".dir").keypress(function(e) {
